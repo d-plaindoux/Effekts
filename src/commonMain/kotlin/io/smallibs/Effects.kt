08a09990c765fed79465258a1b271be3bb8981d0
d-plaindoux <d.plaindoux@free.fr>
2020-12-03 14:42:47 +0100

    Add upper bound for effects

--- src/commonMain/kotlin/io/smallibs/Effects.kt
+++ src/commonMain/kotlin/io/smallibs/Effects.kt
@@ -4,17 +4,17 @@ import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 
-class Effects<O, E>(var effect: suspend (E) -> Any) {                       // O is not used (phantom type)
+class Effects<O, E : Effect<*>>(var effect: suspend (E) -> Any) {                       // O is not used (phantom type)
 
     suspend fun <A> perform(action: E): A = this.effect(action) as A        // Ugly cast remaining here
 
-    class Handler<O, E>(private val code: suspend Effects<O, E>.() -> O) {  
+    class Handler<O, E : Effect<*>>(private val code: suspend Effects<O, E>.() -> O) {
         infix fun with(effects: suspend (E) -> Any): Deferred<O> =
             GlobalScope.async { Effects<O, E>(effects).run { code() } }     // Execution should be reviewed
     }
 
     companion object {
-        fun <O, E> handle(block: suspend Effects<O, E>.() -> O): Handler<O, E> =
+        fun <O, E : Effect<*>> handle(block: suspend Effects<O, E>.() -> O): Handler<O, E> =
             Handler(block)
     }
 }
