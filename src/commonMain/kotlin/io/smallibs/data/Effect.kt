30b6322299e8044c12372a90cac181f328b56db3
d-plaindoux <d.plaindoux@free.fr>
2020-12-22 10:32:41 +0100

    Review code and improve expressivity

--- src/commonMain/kotlin/io/smallibs/data/Effect.kt
+++ src/commonMain/kotlin/io/smallibs/data/Effect.kt
@@ -4,7 +4,7 @@ import io.smallibs.control.App
 import io.smallibs.control.Applicative
 import io.smallibs.control.Functor
 import io.smallibs.control.Monad
-import io.smallibs.data.EffetK.Companion.fix
+import io.smallibs.data.EffetK.Companion.invoke
 import io.smallibs.stdlib.then
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
@@ -13,19 +13,24 @@ data class Internal<A>(val cont: Continuation<A>) : (A) -> Unit {
     override fun invoke(a: A) = cont.resume(a)
 }
 
-class Effect<A>(val behavior: ((A) -> Unit) -> Unit) : App<EffetK, A>
+data class Effect<A>(private val behavior: ((A) -> Unit) -> Unit) : App<EffetK, A>, ((A) -> Unit) -> Unit {
+    override operator fun invoke(a: (A) -> Unit) = behavior(a)
+}
 
 class EffetK private constructor() {
     @Suppress("UNCHECKED_CAST")
     companion object {
         fun <A> App<EffetK, A>.fix(): Effect<A> =
             this as Effect<A>
+
+        operator fun <A> App<EffetK, A>.invoke(a: (A) -> Unit): Unit =
+            this.fix()(a)
     }
 }
 
 class EffectFunctor : Functor<EffetK> {
     override fun <A, B> map(ma: App<EffetK, A>, f: (A) -> B): App<EffetK, B> =
-        Effect { b -> ma.fix().behavior(f then b) }
+        Effect { b -> ma(f then b) }
 }
 
 class EffectApplicative(override val functor: Functor<EffetK> = EffectFunctor()) : Applicative<EffetK> {
@@ -33,11 +38,11 @@ class EffectApplicative(override val functor: Functor<EffetK> = EffectFunctor())
         Effect { k -> k(a) }
 
     override fun <A, B> apply(mf: App<EffetK, (A) -> B>): (App<EffetK, A>) -> App<EffetK, B> = { ma ->
-        Effect { b -> mf.fix().behavior { f -> ma.fix().behavior(f then b) } }
+        Effect { b -> mf { f -> ma(f then b) } }
     }
 }
 
 class EffectMonad(override val applicative: Applicative<EffetK> = EffectApplicative()) : Monad<EffetK> {
     override fun <A> join(mma: App<EffetK, App<EffetK, A>>): App<EffetK, A> =
-        Effect { a -> mma.fix().behavior { ma -> ma.fix().behavior(a) } }
+        Effect { a -> mma { ma -> ma(a) } }
 }
--- src/commonMain/kotlin/io/smallibs/effect/Effects.kt
+++ src/commonMain/kotlin/io/smallibs/effect/Effects.kt
@@ -3,7 +3,7 @@ package io.smallibs.effect
 import io.smallibs.control.App
 import io.smallibs.data.Effect
 import io.smallibs.data.EffetK
-import io.smallibs.data.EffetK.Companion.fix
+import io.smallibs.data.EffetK.Companion.invoke
 import io.smallibs.data.Internal
 import kotlin.coroutines.suspendCoroutine
 
@@ -16,10 +16,10 @@ class Effects<O, H : Handler>(private val block: suspend Effects<O, H>.(H) -> O)
         block(effect)
 
     suspend fun <A> Effect<A>.perform(): A =
-        suspendCoroutine { cont -> this.behavior(Internal(cont)) }
+        suspendCoroutine { this(Internal(it)) }
 
     suspend fun <A> App<EffetK, A>.perform(): A =
-        suspendCoroutine { cont -> this.fix().behavior(Internal(cont)) }
+        suspendCoroutine { this(Internal(it)) }
 
     companion object {
         fun <O, H : Handler> handle(block: suspend Effects<O, H>.(H) -> O) =
